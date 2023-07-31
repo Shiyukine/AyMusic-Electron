502697e1a657177fa99340fef69223e3f1681ade
Shiyukine <benjbres@gmail.com>
2023-07-31 22:48:43 +0200

    updated version

--- update.js
+++ update.js
@@ -18,8 +18,8 @@ async function getFiles(dir) {
 
 var configUpdate = {
     servUrl: "",
-    versionName: "0.1.1",
-    versionCode: 2
+    versionName: "0.1.2",
+    versionCode: 3
 }
 
 function getChecksum(path) {
