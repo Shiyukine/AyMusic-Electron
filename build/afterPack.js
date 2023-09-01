351811fedf6184680ad6567eb6ecd6b815eb1c9e
Shiyukine <benjbres@gmail.com>
2023-09-01 17:38:43 +0200

    you can rebuild on windows

--- build/afterPack.js
+++ build/afterPack.js
@@ -2,16 +2,17 @@ exports.default = function (context) {
   const { execSync } = require('child_process')
   var platform = process.platform
   if (platform == "darwin") platform = "macos"
-  if (platform == "win32") platform = "windows"
+  if (platform == "win32") platform = "win"
   if (platform == "linux") platform = "linux"
-  execSync("cp ./build/updaters/" + platform + "/* ./dist/" + platform + "-unpacked/")
   if (process.platform === 'darwin') {
+    execSync("cp ./build/updaters/" + platform + "/* ./dist/" + platform + "-unpacked/")
     // VMP sign via EVS
     console.log('VMP signing start')
     execSync('py -m castlabs_evs.vmp sign-pkg ./dist/mac ' + context.appOutDir)
     console.log('VMP signing complete')
   }
   else if (process.platform === "linux") {
+    execSync("cp ./build/updaters/" + platform + "/* ./dist/" + platform + "-unpacked/")
     console.log('Creating output md5')
     execSync('python3 ./build/md5output.py linux -B')
     console.log('md5 complete')
