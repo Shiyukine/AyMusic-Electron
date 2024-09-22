58865f72f494132f45cd1218f7f61097ef7e41a0
Shiyukine <benjbres@gmail.com>
2024-09-22 14:21:13 +0200

    updated version

--- update.js
+++ update.js
@@ -18,8 +18,8 @@ async function getFiles(dir) {
 
 var configUpdate = {
     servUrl: "",
-    versionName: "0.3.1",
-    versionCode: 8,
+    versionName: "0.3.2",
+    versionCode: 9,
     isRelease: false,
     closing: false
 }
